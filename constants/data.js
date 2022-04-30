0f5f48eb8b31934c9952e868cf0d222e8f16f3be
Iftikhor Turamuradov <iftixort5t5@gmail.com>
2022-04-30 21:34:55 +0500

    I create Portfolio website

---
+++ constants/data.js
@@ -0,0 +1,120 @@
+const userData = {
+  githubUsername: "IftixorT",
+  name: "Iftikhor Turamuradov",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/hero.png",
+  email: "iftikhort3344@gmail.com",
+  phone: "+998 93 906 33 44",
+  address: "Jomboy, Samarkand, Uzbekistan",
+  projects: [
+    {
+      title: "Hulu cinema",
+      link: "https://hulu-clones.netlify.app/",
+      imgUrl: "/hulu.jpg",
+    },
+    {
+      title: "Amazon Clone",
+      link: "https://amazon-clones.vercel.app/",
+      imgUrl: "/amazon.jpg",
+    },
+    {
+      title: "Portfolio",
+      link: "https://iftixordev.netlify.app/",
+      imgUrl: "/portfolio2.png",
+    },
+    {
+      title: "USale",
+      link: "https://u-sale.netlify.app/",
+      imgUrl: "/talaba.jpg",
+    },
+    {
+      title: "AppLab",
+      link: "https://toogether.netlify.app/",
+      imgUrl: "/applab.png",
+    },
+    {
+      title: "Movie Trailers",
+      link: "https://movie-website1.netlify.app/",
+      imgUrl: "/movie.jpg",
+    },
+    {
+      title: "YouTube Clone",
+      link: "https://youtube-clone-dev.netlify.app/",
+      imgUrl: "/youtube.jpg",
+    },
+    {
+      title: "StartupLanding",
+      link: "https://iftixor-nextjs-project.netlify.app/",
+      imgUrl: "/start.png",
+    },
+    {
+      title: "Tesla Clone",
+      link: "https://tesla-clone-reactjss.netlify.app/",
+      imgUrl: "/tesla.jpeg",
+    },
+    {
+      title: "Loan Amount Technology",
+      link: "https://procoderipp.netlify.app/index%202.html",
+      imgUrl: "/technology.jpeg",
+    },
+    {
+      title: "webcoding",
+      link: "https://iftixorportfolio.netlify.app/",
+      imgUrl: "/webcoding.jpeg",
+    },
+    {
+      title: "Supreme",
+      link: "https://toogether-tech-project.vercel.app/",
+      imgUrl: "/supreme.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer 🚀, that loves building products and web applications that impact millions of lives.",
+
+    description: [
+      `I’ve developed a lot of apps and websites since I was 13 years old. At the time, I didn’t know what a full stack meant because the term wasn’t developed at the time.
+      After learning HTML and struggling with CSS, I came up with a great idea to use bootstrap so I don't have to style everything independently, and I learned how to create interactive websites by learning the JavaScript programming language, and for obvious reasons, if you know bootstrap, great.`,
+      `I can independently React, Svelte, Node.js, Express.js, Fastify, MongoDB, MySQL, Bootstrap, Tailwind-css,
+      Sass, jQuery, React Hooks, Axios, Redux, Redux-saga, NPM, Yarn, Postman, Figma, Restful Api, Json, Trello, Jira, Git, GitHub, Next.js, Firebase, AWS, Netlify.`,
+      `I learned algorithms and data structures by coding from the following websites and so far I am learning LeetCode, algoexpert, hackerrank.com.`,
+    ],
+
+    
+    // currentProject: "Tailwind Master Kit",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Project manager",
+      company: "Toogether Tech Company",
+      year: "2022",
+      companyLink: "https://toogether.it/",
+      desc: "I worked for the company as a Project Manager in the Frontend Group. I worked for this company for 4 months until the project was completed.",
+    },
+    {
+      title: "Frontend Developer",
+      company: "Toogether Tech Company",
+      year: "2021",
+      companyLink: "https://toogether.it/",
+      desc: "I have been working for a company as a Web application developer. The company has a very good communion. All programmers work as one.",
+    },
+    {
+      title: "Frontend Developer",
+      company: "Galactic game zone",
+      year: "2020",
+      companyLink: "https://gamesgalactic.com/",
+      desc: "I’ve done a lot of great things in this job. I worked responsibly and interactively on the website.",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1M-xJiWdirNG8UWq6IAS7CoHp4-YI4uUx/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/iftixor001",
+    linkedin: "https://linkedin.com/in/iftixordev",
+    github: "https://github.com/IftixorT",
+    facebook: "https://www.facebook.com/iftixor.turamuradov",
+  },
+};
+
+export default userData;
